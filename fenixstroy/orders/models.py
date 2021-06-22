5dc90758379cd0e3241593db96e0fa5c1c64087a
wiky-avis <peta-holding@yandex.ru>
2021-06-22 18:15:50 +0300

    включение моделей заказов на сайте администрирования

--- fenixstroy/orders/models.py
+++ fenixstroy/orders/models.py
@@ -26,8 +26,12 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items')
-    product = models.ForeignKey(Gloves, related_name='order_items')
+    order = models.ForeignKey(
+        Order, related_name='items', on_delete=models.CASCADE
+        )
+    product = models.ForeignKey(
+        Gloves, related_name='order_items', on_delete=models.CASCADE
+        )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
