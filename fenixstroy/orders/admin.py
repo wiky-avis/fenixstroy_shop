339afd62ff2eda4971a3959017efa8920ae240e6
Виктория Аксентий <peta-holding@yandex.ru>
2021-06-23 21:58:58 +0300

    преобразование счетов в pdf

--- fenixstroy/orders/admin.py
+++ fenixstroy/orders/admin.py
@@ -5,6 +5,13 @@ from django.contrib import admin
 from django.http import HttpResponse
 
 from .models import Order, OrderItem
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(
+        reverse('orders:admin_order_detail', args=[obj.id])))
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -13,9 +20,9 @@ def export_to_csv(modeladmin, request, queryset):
     response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
     writer = csv.writer(response)
     fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
-    # Напишите первый ряд с информацией заголовка
+    # Записываем первую строку с заголовками полей.
     writer.writerow([field.verbose_name for field in fields])
-    # Написать строки данных
+    #  Записываем данные.
     for obj in queryset:
         data_row = []
         for field in fields:
@@ -35,13 +42,22 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_pdf(obj):
+    return mark_safe(
+        '<a href="{}">PDF</a>'.format(
+            reverse('orders:admin_order_pdf', args=[obj.id])
+            )
+        )
+
+
+order_pdf.short_description = 'Invoice'
+
+
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
-    list_filter = ['paid', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated', order_detail, order_pdf]
     inlines = [OrderItemInline]
     actions = [export_to_csv]
-
-
-admin.site.register(Order, OrderAdmin)
--- fenixstroy/orders/views.py
+++ fenixstroy/orders/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from .models import OrderItem
-from .forms import OrderCreateForm
+import weasyprint
 from cart.cart import Cart
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import get_object_or_404
-from .models import Order
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template.loader import render_to_string
+
+from .forms import OrderCreateForm
+from .models import Order, OrderItem
 
 
 @staff_member_required
@@ -36,3 +39,15 @@ def order_create(request):
     return render(
         request, 'orders/order/create.html', {'cart': cart, 'form': form}
         )
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
