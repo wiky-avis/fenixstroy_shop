03d4c2d2d3089c5e7699b2dfb2ec22d832d52924
wiky-avis <peta-holding@yandex.ru>
2021-06-24 12:13:57 +0300

    pdf delete

--- fenixstroy/orders/admin.py
+++ fenixstroy/orders/admin.py
@@ -5,13 +5,6 @@ from django.contrib import admin
 from django.http import HttpResponse
 
 from .models import Order, OrderItem
-from django.urls import reverse
-from django.utils.safestring import mark_safe
-
-
-def order_detail(obj):
-    return mark_safe('<a href="{}">View</a>'.format(
-        reverse('orders:admin_order_detail', args=[obj.id])))
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -42,22 +35,11 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
-def order_pdf(obj):
-    return mark_safe(
-        '<a href="{}">PDF</a>'.format(
-            reverse('orders:admin_order_pdf', args=[obj.id])
-            )
-        )
-
-
-order_pdf.short_description = 'Invoice'
-
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
-    list_filter = ['paid', 'created', 'updated', order_detail, order_pdf]
+    list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- fenixstroy/orders/views.py
+++ fenixstroy/orders/views.py
@@ -1,10 +1,6 @@
-import weasyprint
 from cart.cart import Cart
-from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.template.loader import render_to_string
 
 from .forms import OrderCreateForm
 from .models import Order, OrderItem
@@ -39,15 +35,3 @@ def order_create(request):
     return render(
         request, 'orders/order/create.html', {'cart': cart, 'form': form}
         )
-
-
-@staff_member_required
-def admin_order_pdf(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    html = render_to_string('orders/order/pdf.html', {'order': order})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(order.id)
-    weasyprint.HTML(string=html).write_pdf(
-        response,
-        stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
-    return response
