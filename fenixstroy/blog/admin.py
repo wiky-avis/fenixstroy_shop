9b7e9df78e8f89ec975e087a1f8ada62bace7630
wiky-avis <peta-holding@yandex.ru>
2021-07-06 11:53:33 +0300

    модели блога

--- fenixstroy/blog/admin.py
+++ fenixstroy/blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category, Article, ArticleComment
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    empty_value_display = '-пусто-'
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'pub_date', 'category', 'image')
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    empty_value_display = '-пусто-'
+
+
+class ArticleCommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'article', 'text', 'created')
+    list_display_links = ('text',)
+    search_fields = ('article',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleComment, ArticleCommentAdmin)
--- fenixstroy/blog/models.py
+++ fenixstroy/blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=70, verbose_name='Название категории')
+    slug = models.SlugField(verbose_name='Слаг')
+
+    class Meta:
+        ordering = ('-name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Статья')
+    slug = models.SlugField(verbose_name='Слаг')
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата публикации')
+    text = models.TextField(verbose_name='Текст поста')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        null=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+class ArticleComment(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='article_comments', verbose_name='Статья')
+    text = models.TextField(
+        verbose_name='Комментарий',
+        help_text='Введите пожалуйста текст вашего комментария')
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='articles/',
+        null=True,
+        blank=True, default='default/def.jpg',)
+    created = models.DateTimeField(
+        verbose_name='Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
